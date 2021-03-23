61331570cd7eea68d5f613becd281f4e0848112a
GayatriGiramkar <gayatri.giramkar93@gmail.com>
2021-03-23 10:58:11 +0530

    Pushing to remote repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import { AgmCoreModule } from '@agm/core';
+// import { google } from "google-maps";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey:'AIzaSyAFgM81Qz-SwfTzUsr4F51AgDj0HdN88CQ'
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
